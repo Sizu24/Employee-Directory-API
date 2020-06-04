fba0afec4804aee0b05ae09467ef31b61d563b4a
Shahetul Chisty <sizu1124@gmail.com>
2020-06-03 19:55:59 -0700

    Layout, and JS code for fetch API

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let listContainer = document.querySelector('main');
+let userinfo = [];
+
+for(let i = 0; i < 12; i++){
+  getProfiles('https://randomuser.me/api/');
+}
+
+function getProfiles(url){
+  fetch(url)
+  .then(response => response.json())
+  // data.results[0]
+  // picture.thumbnail or large or medium
+  // name, email, location.city
+  .then(data => userinfo = (data.results[0]))
+  .then(arr => getArray(arr))
+  .then(profile => display(profile))
+  .then(listItems => listContainer.appendChild(listItems))
+  .catch(error=> console.log('There was an error!', error));
+}
+
+let getArray = (info)=>{
+  let names = [];
+  names.push(`<img src=${info.picture.thumbnail} alt="profile-pic">`);
+  names.push(`<h2>${info.name.first} ${info.name.last}</h2>`);
+  names.push(`<p>${info.email}</p>`);
+  names.push(`<p>${info.location.city}</p>`);
+  return names;
+}
+
+let display = (array)=>{
+  let list = document.createElement('ul');
+  let listItem = "";
+  array.forEach(item => {
+    listItem += `<li>${item}</li>`;
+    list.innerHTML = listItem;
+  });
+  return list;
+}
+
+

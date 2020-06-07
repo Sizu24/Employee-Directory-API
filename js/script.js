a2f9ba397217956494c3e238b738b6a377ecf165
Shahetul Chisty <sizu1124@gmail.com>
2020-06-06 23:56:06 -0700

    modal pop up, new js code for displaying profiles

--- js/script.js
+++ js/script.js
@@ -1,42 +1,78 @@
-let listContainer = document.querySelector('main');
-let userinfo = [];
+const listContainer = document.querySelector(".card-container");
+let employees = [];
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
+const url = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`;
 
-for(let i = 0; i < 12; i++){
-  getProfiles('https://randomuser.me/api/');
-}
+fetch(url)
+.then(response => response.json())
+.then(response => response.results)
+.then(profile => display(profile))
+.catch(error=> console.log('There was an error!', error));
 
-function getProfiles(url){
-  fetch(url)
-  .then(response => response.json())
-  // data.results[0]
-  // picture.thumbnail or large or medium
-  // name, email, location.city
-  .then(data => userinfo = (data.results[0]))
-  .then(arr => getArray(arr))
-  .then(profile => display(profile))
-  .then(listItems => listContainer.appendChild(listItems))
-  .catch(error=> console.log('There was an error!', error));
-}
+function display(employeeData){
+  employees = employeeData;
+  console.log(employees);
+  let employeeHTML = "";
+  employees.forEach((employee, index) =>{
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
 
-let getArray = (info)=>{
-  let names = [];
-  names.push(`<img src=${info.picture.large} alt="profile-pic">`);
-  names.push(`<h2>${info.name.first} ${info.name.last}</h2>`);
-  names.push(`<p>${info.email}</p>`);
-  names.push(`<p>${info.location.city}</p>`);
-  return names;
-}
+    employeeHTML += `
+    <div class="card" data-index="${index}">
+    <img class="avatar" src="${picture.large}" alt="profile pic">
+    <div class="text-container">
+      <h2 class="name">${name.first} ${name.last}</h2>
+      <p class="email">${email}</p>
+      <p class="address">${city}</p>
+    </div>
+  </div>
+    `;
 
-// if info.email.length > 26, p.style.paddingRight = "10px";
-
-let display = (array)=>{
-  let list = document.createElement('ul');
-  let listItem = "";
-  array.forEach(item => {
-    listItem += `<li>${item}</li>`;
-    list.innerHTML = listItem;
   });
-  return list;
+  listContainer.innerHTML = employeeHTML;
 }
+
+function displayModal (index){
+  let {name, dob, phone, email, location:
+     { city, street, state, postcode}, picture}
+      = employees[index];
+
+  let date = new Date(dob.date);
+
+const modalHTML = `
+<img class="avatar" src="${picture.large}" alt="profile pic">
+<div class="modal-text">
+  <h2 class="name">${name.first} ${name.last}</h2>
+  <p class="email">${email}</p>
+  <p class="address">${city}</p>
+  <hr />
+  <p>${phone}</p>
+  <p class="address">${street.number} ${street.name} ${state} ${postcode}</p>
+  <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+</div>
+`;
+
+overlay.classList.remove("hidden");
+modalContainer.innerHTML = modalHTML;
+}
+
+listContainer.addEventListener("click", e =>{
+  if(e.target !== listContainer){
+    console.log("clicked");
+    const card = e.target.closest(".card");
+    const index = card.getAttribute("data-index");
+  
+  displayModal(index);
+  }
+});
+
+modalClose.addEventListener("click", () =>{
+  overlay.classList.add("hidden");
+});
 
 

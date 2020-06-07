55e11fdcc00852f5e026ed0abf96344ce413e65d
Shahetul Chisty <sizu1124@gmail.com>
2020-06-07 16:34:24 -0700

    Comments in JS, normalize, padding in title

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Global Variables 
 const listContainer = document.querySelector(".card-container");
 let employees = [];
 const overlay = document.querySelector(".overlay");
@@ -7,14 +8,22 @@ const url = `https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`;
 
 fetch(url)
+/*
+  Get api from server, 
+  take results property and copy to response,
+  send data to display funciton
+*/
 .then(response => response.json())
 .then(response => response.results)
 .then(profile => display(profile))
 .catch(error=> console.log('There was an error!', error));
 
 function display(employeeData){
+  // copy data to employees array
   employees = employeeData;
-  console.log(employees);
+  // console.log(employees);
+
+  // assign data
   let employeeHTML = "";
   employees.forEach((employee, index) =>{
     let name = employee.name;
@@ -22,6 +31,7 @@ function display(employeeData){
     let city = employee.location.city;
     let picture = employee.picture;
 
+    // setup HTML code and store it in variable
     employeeHTML += `
     <div class="card" data-index="${index}">
     <img class="avatar" src="${picture.large}" alt="profile pic">
@@ -34,14 +44,16 @@ function display(employeeData){
     `;
 
   });
+  // add HTML code to card container in HTML
   listContainer.innerHTML = employeeHTML;
 }
 
 function displayModal (index){
+  // create object to hold info
   let {name, dob, phone, email, location:
      { city, street, state, postcode}, picture}
       = employees[index];
-
+  // Data object for dob
   let date = new Date(dob.date);
 
 const modalHTML = `
@@ -52,25 +64,27 @@ const modalHTML = `
   <p class="address">${city}</p>
   <hr />
   <p>${phone}</p>
-  <p class="address">${street.number} ${street.name} ${state} ${postcode}</p>
+  <p class="address">${street.number} ${street.name} ${city}, ${state} ${postcode}</p>
   <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
 </div>
 `;
 
+// remove hidden class to display overlay of modal, and add modal HTML data
 overlay.classList.remove("hidden");
 modalContainer.innerHTML = modalHTML;
 }
 
 listContainer.addEventListener("click", e =>{
+  // find closest card, and get index of that card
   if(e.target !== listContainer){
-    console.log("clicked");
     const card = e.target.closest(".card");
     const index = card.getAttribute("data-index");
-  
+  // run function to display data from that specific index of card
   displayModal(index);
   }
 });
 
+// Close modal
 modalClose.addEventListener("click", () =>{
   overlay.classList.add("hidden");
 });
